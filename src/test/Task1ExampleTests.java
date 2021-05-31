5a81db2cda51a0252eee0e5fef11f4b7369819fa
Braedon Wooding <b.wooding@unsw.edu.au>
2021-05-31 17:53:28 +1000

    Update src/test/Task1ExampleTests.java, src/test/Task2ExampleTests.java files

--- src/test/Task1ExampleTests.java
+++ src/test/Task1ExampleTests.java
@@ -12,52 +12,56 @@ import test.test_helpers.TestHelper;
 public class Task1ExampleTests {
    @Test
    public void testExample() {
-       // Task 1
-       // Example from the specification
+        // Task 1
+        // Example from the specification
        
-       // Creates 1 satellite and 3 devices
-       // 2 devices are in view of the satellite
-       // 1 device is out of view of the satellite
+        // Creates 1 satellite and 3 devices
+        // 2 devices are in view of the satellite
+        // 1 device is out of view of the satellite
+        String initialWorldState = new ResponseHelper(LocalTime.of(0, 0)) 
+                // note: all doubles are to 0.01 precision
+                // so 141.66 == 141.67.
+                .expectSatellite("BlueOriginSatellite", "Satellite1", 10000, 340, 141.66,
+                    /* Possible Connections */ new String[] { "DeviceA", "DeviceC" })
+                .expectDevice("HandheldDevice", "DeviceA", 30)
+                .expectDevice("LaptopDevice", "DeviceB", 180)
+                .expectDevice("DesktopDevice", "DeviceC", 330)
+                .toString();
 
-       // Shows the world state, then moves a device, then removes a device.
+        // Then after moving DeviceA to theta = 211 the world state should be
+        String afterMoveWorldState = new ResponseHelper(LocalTime.of(0, 0))
+            .expectSatellite("BlueOriginSatellite", "Satellite1", 10000, 340, 141.66, new String[] { "DeviceC" })
+            .expectDevice("HandheldDevice", "DeviceA", 211) // position has changed
+            .expectDevice("LaptopDevice", "DeviceB", 180)
+            .expectDevice("DesktopDevice", "DeviceC", 330)
+            .toString();
 
-       // this is what we call the 'builder' pattern, effectively we scope out a test plan in this case
-       // by asking the TestHelper to create a series of devices/satellites, then we ask it to show the world state
-       // and we use a *different* builder pattern to state how we want the response to look (i.e. expect all the devices/satellites we created before).
+        String afterRemovingWorldState = new ResponseHelper(LocalTime.of(0, 0))
+            .expectSatellite("BlueOriginSatellite", "Satellite1", 10000, 340, 141.66, new String[] { "DeviceC" })
+            .expectDevice("HandheldDevice", "DeviceA", 211)
+            .expectDevice("DesktopDevice", "DeviceC", 330)
+            // notice no B
+            .toString();
 
-       // Since we want you to model your own device/satellite code this is a bit overly 'generic', normally you would probably concrete this a bit
-       // i.e. createDevice(new Device(...)) but in this case this is still fine / readable.
-       // Unsure what a function does?  Just mouse over it, and you'll get a nice description including sample json output.
-       TestHelper plan = new TestHelper()
+        // this is what we call the 'builder' pattern, effectively we scope out a test plan in this case
+        // by asking the TestHelper to create a series of devices/satellites, then we ask it to show the world state
+        // and we use a *different* builder pattern to state how we want the response to look (i.e. expect all the devices/satellites we created before).
+
+        // Since we want you to model your own device/satellite code this is a bit overly 'generic', normally you would probably concrete this a bit
+        // i.e. createDevice(new Device(...)) but in this case this is still fine / readable.
+        // Unsure what a function does?  Just mouse over it, and you'll get a nice description including sample json output.
+        TestHelper plan = new TestHelper()
             .createDevice("HandheldDevice", "DeviceA", 30)
             .createDevice("LaptopDevice", "DeviceB", 180)
             .createDevice("DesktopDevice", "DeviceC", 330)
             .createSatellite("BlueOriginSatellite", "Satellite1", 10000, 340)
-            .showWorldState(new ResponseHelper(LocalTime.of(0, 0)) 
-                // note: all doubles are to 0.01 precision
-                // so 141.66 == 141.67.
-                .expectSatellite("BlueOriginSatellite", "Satellite1", 10000, 340, 141.66, new String[] { "DeviceA", "DeviceC" })
-                .expectDevice("HandheldDevice", "DeviceA", 30)
-                .expectDevice("LaptopDevice", "DeviceB", 180)
-                .expectDevice("DesktopDevice", "DeviceC", 330)
-                .toString()
-            )
+            .showWorldState(initialWorldState)
             .moveDevice("DeviceA", 211)
-            .showWorldState(new ResponseHelper(LocalTime.of(0, 0))
-                .expectSatellite("BlueOriginSatellite", "Satellite1", 10000, 340, 141.66, new String[] { "DeviceC" })
-                .expectDevice("HandheldDevice", "DeviceA", 211)
-                .expectDevice("LaptopDevice", "DeviceB", 180)
-                .expectDevice("DesktopDevice", "DeviceC", 330)
-                .toString()
-            ).removeDevice("DeviceB")
-            .showWorldState(new ResponseHelper(LocalTime.of(0, 0))
-                .expectSatellite("BlueOriginSatellite", "Satellite1", 10000, 340, 141.66, new String[] { "DeviceC" })
-                .expectDevice("HandheldDevice", "DeviceA", 211)
-                .expectDevice("DesktopDevice", "DeviceC", 330)
-                .toString()
-            );
+            .showWorldState(afterMoveWorldState)
+            .removeDevice("DeviceB")
+            .showWorldState(afterRemovingWorldState);
 
         // this is the magic!  DON'T forget this, you need the execute else it won't actually run any tests!
         plan.executeTestPlan();
-   }
+    }
 }
